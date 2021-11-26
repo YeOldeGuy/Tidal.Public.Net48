3d2348b4d6218abe472efcf79ba530ae4c85ab87
YeOldeGuy <56144259+YeOldeGuy@users.noreply.github.com>
2021-11-26 13:31:55 -0800

    Fixed a formatting error in the Peers column

---
+++ Tidal/Converters/KConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Tidal.Converters
+{
+    internal class KConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int iVal)
+            {
+                if (iVal > 5000)
+                    return "5K+";
+                else if (iVal > 4000)
+                    return "4K+";
+                else if (iVal > 3000)
+                    return "3K+";
+                else if (iVal > 2000)
+                    return "2K+";
+                else if (iVal > 1000)
+                    return "1K+";
+                else
+                    return iVal.ToString();
+            }
+            return value.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
